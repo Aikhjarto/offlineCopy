a45cbb01edc22941c760c6cb6e52212c51d36792
Aikhjarto <wagner-thomas@gmx.at>
2013-07-11 23:41:39 +0200

    more sane defaults

--- settings.php
+++ settings.php
@@ -4,12 +4,12 @@ $f1="left.list"; # file that was produced with "find . > left.list" in source fo
 $f2="right.list";# file that was produced with "find . > right.list" in destination folder
 
 # folders on source machine
-$prefixF1="/pub/Movies/Movies"; # should be the folder where "find ." was executed in source folder
+$prefixF1="/path/to/mySrcData"; # should be the folder where "find ." was executed in source folder
 $tmpFolderF1="/backup/tmp"; # folder on your external harddrive as mounted on the source machine
 
 
 # folders on destination machine
-$prefixF2="/pub/newMovies"; # should be the folder where "find ." was executed in destination folder
+$prefixF2="/path/to/myDstData"; # should be the folder where "find ." was executed in destination folder
 $tmpFolderF2="/backup/tmp"; # folder on your external harddrive as mounted on the destination machine
 
 ?>
